7b498e49303b4e3b62f3bd11079c771f1b31ab55
james <g00232918@gmit.ie>
2023-12-21 21:56:15 +0000

    dbconfig update

---
+++ dbconfigpa.py
@@ -0,0 +1,6 @@
+mysql = {
+    'host':"jamesconnolly147.mysql.pythonanywhere-services.com",
+    'user':"jamesconnolly147",
+    'password':"Jimmy147",
+    'database':"jamesconnolly147$datarepresentation"
+}
